1fcb2a107c931bd8d9bccdc812c9eb0c4ed3cd8a
peeraphat19 <peeraphat.sujarit@gmail.com>
2024-10-31 08:59:59 +0700

    com

--- Programming/OnlineShop_bootstrap/shop.js
+++ Programming/OnlineShop_bootstrap/shop.js
@@ -9,21 +9,21 @@ const products = [
       id: 1,
       name: "MOON NO.1",
       price: "$99",
-      image: "/Programming/OnlineShop_bootstrap/pic/_DSC9509-1.jpg",
+      image: "/Programming/OnlineShop_bootstrap/pic/Moon.jpg",
       description: "The moon of EARTH-616 (The Prime Universe)"
     },
     {
       id: 2,
       name: "MOON NO.2",
       price: "$120",
-      image: "/Programming/OnlineShop_bootstrap/pic/_DSC9509-1.jpg",
+      image: "/Programming/OnlineShop_bootstrap/pic/Moon.jpg",
       description: "The moon of EARTH-717 (What If ?)"
     },
     {
         id: 3,
         name: "MOON NO.3",
         price: "$120",
-        image: "/Programming/OnlineShop_bootstrap/pic/_DSC9509-1.jpg",
+        image: "/Programming/OnlineShop_bootstrap/pic/Moon.jpg",
         description: "The moon of EARTH-78411 (Dinosaur World)"
       },
     
@@ -31,7 +31,7 @@ const products = [
         id: 4,
         name: "MOON NO.4",
         price: "$75",
-        image: "/Programming/OnlineShop_bootstrap/pic/_DSC9509-1.jpg",
+        image: "/Programming/OnlineShop_bootstrap/pic/Moon.jpg",
         description: "The moon of EARTH-2149 (Zombiverse)"
       },
     
@@ -39,14 +39,14 @@ const products = [
         id: 5,
         name: "MOON NO.5",
         price: "$120",
-        image: "/Programming/OnlineShop_bootstrap/pic/_DSC9509-1.jpg",
+        image: "/Programming/OnlineShop_bootstrap/pic/Half.jpg",
         description: "The moon of EARTH-928 (2099 A.D.)"
       },
       {
         id: 6,
         name: "HALF MOON NO.0",
         price: "$130",
-        image: "/Programming/OnlineShop_bootstrap/pic/_DSC9509-1.jpg",
+        image: "/Programming/OnlineShop_bootstrap/pic/Half.jpg",
         description: "???"
       },
       
